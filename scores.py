b620e8a31e8a3ad7b3c848d1df9c46694bd3a8bc
skynettw <skynet.tw@gmail.com>
2022-10-24 17:10:25 +0800

    score management program

--- scores.py
+++ scores.py
@@ -8,22 +8,29 @@ while not quit:
     print("3. Remove student's data")
     print("4. Print the Average/Max/Min data for the scores")
     print("5. List all students' data")
-    print("6. End the program")
+    print("6. Save scores to a file")
+    print("7. Load scores form a file")
+    print("0. End the program")
     print("------------------")
     res = input("Please input your choice:")
     if res == "1":
         data.clear()
         names.clear()
     elif res == "2":
-        s = int(input("Score="))
-        n = input("Name=")
-        data.append(s)
-        names.append(n)
+        s = int(input("Score(-1 to end)="))
+        while s>0:
+            n = input("Name=")
+            data.append(s)
+            names.append(n)
+            s = int(input("Score(-1 to end)="))
     elif res == "3":
         n = input("Name=")
-        i = names.index(n)
-        del names[i]
-        del data[i]
+        try:
+            i = names.index(n)
+            del names[i]
+            del data[i]
+        except:
+            pass
     elif res == "4":
         print("Max score is ", max(data))
         print("Min score is ", min(data))
@@ -32,7 +39,24 @@ while not quit:
         score_table = zip(names, data)
         for item in score_table:
             print(item)
-    elif res == "6":
+    elif res == "6": # save the scores to a file
+        filename = input("Filename:")
+        fp = open(filename, "w", encoding="utf-8")
+        score_table = zip(names, data)
+        for item in score_table:
+            fp.write(item[0]+","+str(item[1])+"\n")
+        fp.close()
+    elif res == "7": # load the scores from a file
+        filename = input("Filename:")
+        fp = open(filename, "r", encoding="utf-8")
+        rawdata = fp.readlines()
+        fp.close()
+        names.clear()
+        data.clear()
+        for item in rawdata:
+            names.append(item.split(",")[0])
+            data.append(int(item.split(",")[1]))
+    elif res == "0":  # quit the program
         quit = True
     else:
         print("I can't recognize your command!")
