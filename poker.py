0d8087634d7acaea89d6a91f5fd2b8b18a335462
skynettw <skynet.tw@gmail.com>
2022-11-04 12:11:43 +0800

    add some comments to poker.py

--- poker.py
+++ poker.py
@@ -1,10 +1,16 @@
 import random
 
+#建立一個card_face串列，用來放所有牌面的花色，以下總共52個項目（每個花色各13個）
 card_face = ["Diamond"]*13 + ["Spade"]*13 + ["Heart"] * 13 + ["Club"] * 13
+#建立一個card_value串列，用來放所有牌面的點數，以下總共52個項目（每個花色各13個值，分別是1~13）
 card_value = [i for i in range(1, 14)] * 4
+#把前面兩個串列組合成一個串列deck，用來放所有的撲克牌內容（依照順序放），內容分別是('Diamond', 1), ('Diamond', 2), ..., ('Club', 13)
 deck = [item for item in zip(card_face, card_value)]
+#把deck串列中的牌順序打亂
 random.shuffle(deck)
+#建立一個用來存放玩家手上的牌的串列player_cards
 player_cards = list()
+#把deck串列中的最後1張牌pop出來，放到player_cards串列中
 player_cards.append(deck.pop())
 print("Your cards:", player_cards)
 answer = input("請問你要補牌嗎？")
